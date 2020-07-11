d28022283f8fe23cf55ab449e461f9c95847de99
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-11 15:49:44 +0300

    Add a databaseRule class to dbRule package and override before and after functions inside it with code for connecting tests to test database and deleting the database data after the test has run

---
+++ src/test/java/dbRule/DatabaseRule.java
@@ -0,0 +1,21 @@
+package dbRule;
+
+import org.junit.rules.ExternalResource;
+import models.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource {
+    @Override
+    protected void before(){
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "vincent", "Taptet#2001");
+    }
+
+    @Override
+    protected  void after () {
+        try (Connection con = DB.sql2o.open()){
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+        }
+    }
+}
