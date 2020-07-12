0036f870272af8f1f65062f5433df1b61091eea7
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-12 16:45:47 +0300

    Add equal and hashcode and save method to Sighting class and their tests in SightingTest class

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,6 +1,10 @@
 package models;
 
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class Sighting {
     private int id;
@@ -23,9 +27,7 @@ public class Sighting {
         return location;
     }
 
-    public String getRangerName() {
-        return rangerName;
-    }
+    public String getRangerName() { return rangerName; }
 
     public int getAnimalId() {
         return animalId;
@@ -33,5 +35,35 @@ public class Sighting {
 
     public Timestamp getCreatedAt() {
         return createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sighting sighting = (Sighting) o;
+        return id == sighting.id &&
+                animalId == sighting.animalId &&
+                Objects.equals(location, sighting.location) &&
+                Objects.equals(rangerName, sighting.rangerName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, location, rangerName, animalId);
+    }
+
+    public void save() {
+        String sql = "INSERT INTO sightings (location, ranger_name, animal_id) VALUES (:location, :ranger_name, :animal_id);";
+        try(Connection con = DB.sql2o.open()) {
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("location", this.getLocation())
+                    .addParameter("ranger_name", this.getRangerName())
+                    .addParameter("animal_id", this.getAnimalId())
+                    .executeUpdate()
+                    .getKey();
+        }catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 }
--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -1,10 +1,15 @@
 package models;
 
+import dbRule.DatabaseRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SightingTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void newSighting_newSightingInstantiatesCorrectly() {
@@ -28,6 +33,28 @@ public class SightingTest {
     public void getAnimalId_newSightingInstantiatesCorrectlyWithAnimalId() {
         Sighting newSighting = setNewSighting();
         assertEquals(1, newSighting.getAnimalId());
+    }
+
+    @Test
+    public void equal_sightingsWithSameLocationRangerNameAndAnimalIdAreEqual() {
+        Sighting  firstSighting = setNewSighting();
+        Sighting  secondSighting = setNewSighting();
+        assertTrue( firstSighting.equals(secondSighting));
+    }
+
+    @Test
+    public void save_sightingIsSavedCorrectlyOnEntry() {
+        Sighting  firstSighting = setNewSighting();
+        firstSighting.save();
+        assertEquals(true, Sighting.all().get(0).equals(firstSighting));
+    }
+
+    @Test
+    public void save_assignsIdToSighting() {
+        Sighting  firstSighting = setNewSighting();
+        firstSighting.save();
+        Sighting savedSighting = Sighting.all().get(0);
+        assertEquals(savedSighting.getId(), firstSighting.getId());
     }
 
     private Sighting setNewSighting(){
