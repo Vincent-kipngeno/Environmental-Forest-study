cc19a37bb2336cf8cb753beb1b0dba9bd07d54f6
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-13 17:00:02 +0300

    Add get routes for displaying list of sightings, animals and endangered animals, display forms for creating new endangered and animal forms, and post methods for processing the forms

---
+++ src/main/java/App.java
@@ -0,0 +1,82 @@
+import models.*;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class App{
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
+        //show all listed sightings and both endangered and non endangered animals
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Animal> animals = Animal.all();
+            model.put("animals", animals);
+            List<EndangeredAnimal> endangeredAnimals = EndangeredAnimal.allEndangered();
+            model.put("endangeredAnimals", endangeredAnimals);
+            List<Sighting> sightings = Sighting.all();
+            model.put("sightings", sightings);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine() );
+
+        //show form to create a new animal
+        get("/animals/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "Animal-form.hbs");
+        }, new HandlebarsTemplateEngine() );
+
+        //process a form to create new animal
+        post("/animals", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String animalName = req.queryParams("name");
+            Animal newAnimal = new Animal(animalName);
+            newAnimal.save();
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine() );
+
+        //show form to create new endangered animal
+        get("/endangered/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "Endangered-form.hbs");
+        }, new HandlebarsTemplateEngine() );
+
+        //process a form to create new endangered animal
+        post("/endangered", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String endangeredName = req.queryParams("name");
+            String health = req.queryParams("health");
+            String age = req.queryParams("age");
+            EndangeredAnimal newEndangeredAnimal = new EndangeredAnimal(endangeredName, health, age);
+            newEndangeredAnimal.save();
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine() );
+
+        //delete all animals and endangered animals and sightings
+        get("/animals/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            Animal.clearAll();
+            EndangeredAnimal.clearAll();
+            Sighting.clearAll();
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine() );
+
+        //delete individual animal
+    }
+}
